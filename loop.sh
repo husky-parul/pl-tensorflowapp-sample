b6f26d411252eaed843dd05cf74dfa68ba2f6c5c
husky-parul <parsingh@redhat.com>
2019-07-30 11:14:43 -0400

    correct location

--- loop.sh
+++ loop.sh
@@ -6,4 +6,8 @@ ls -la /opt/app-root/src/output/mnist_saved_model/1/
 
 ls -la /opt/app-root/src/tensorflowapp-sample/
 ls -la /opt/app-root/src/input/test/
-python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name my_saved_model /opt/app-root/src/output /opt/app-root/src/output
+
+echo 'Test tensor flow';
+python /utils/test.py
+
+python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name mnist_saved_model /opt/app-root/src/output /opt/app-root/src/output
