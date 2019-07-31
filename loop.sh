9829a8c78f3588c9e04e73cfce66fc8e0ed59839
husky-parul <parsingh@redhat.com>
2019-07-31 12:15:16 -0400

    upgrade pip

--- loop.sh
+++ loop.sh
@@ -10,7 +10,7 @@ ls -la /opt/app-root/src/input/test/
 # echo 'Test tensor flow';
 # python /utils/test.py
 
-# python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name mnist_saved_model /opt/app-root/src/output /opt/app-root/src/output
+python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name mnist_saved_model /opt/app-root/src/output /opt/app-root/src/output
 
 
 while true; do echo ".."; sleep 2; done
