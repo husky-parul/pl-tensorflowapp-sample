04a62aac281b549d9b7e87dae63deb3160612ffd
husky-parul <parsingh@redhat.com>
2019-07-31 08:30:31 -0400

    tf whl plyg

--- loop.sh
+++ loop.sh
@@ -7,7 +7,10 @@ ls -la /opt/app-root/src/output/mnist_saved_model/1/
 ls -la /opt/app-root/src/tensorflowapp-sample/
 ls -la /opt/app-root/src/input/test/
 
-echo 'Test tensor flow';
-python /utils/test.py
+# echo 'Test tensor flow';
+# python /utils/test.py
 
-python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name mnist_saved_model /opt/app-root/src/output /opt/app-root/src/output
+# python /opt/app-root/src/tensorflowapp-sample/tensorflowapp-inference.py --prefix mnist- --inference_path /opt/app-root/src/input/test/test.png --saved_model_name mnist_saved_model /opt/app-root/src/output /opt/app-root/src/output
+
+
+while true; do echo ".."; sleep 2; done
